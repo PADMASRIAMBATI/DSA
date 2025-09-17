972ae86acdacaa5f4e15ab451411cbfe0a78dbbe
Padmasri Ambati <143399502+PADMASRIAMBATI@users.noreply.github.com>
2025-09-17 18:08:21 +0530

    Delete Problem Solving directory

--- Problem Solving/2 Math Concepts/1.Reverse number.py
+++
@@ -1,6 +0,0 @@
-N= 7789
-
-while N>0:
-    last_num=N%10
-    N=N//10
-    print(last_num, end='')
--- Problem Solving/3 Recursion/6 fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    last = fibonacci(n-1)
-    sec_last = fibonacci(n-2)
-    return last + sec_last
-n = 7
-print(fibonacci(n))
